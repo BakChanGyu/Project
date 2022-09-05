13747b5bdf4bcb84e8d0a84251f454fc5ada2b07
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2022-09-05 14:29:48 +0900

    Add files via upload

---
+++ Project/src/main/java/project/repository/MemberRepository.java
@@ -0,0 +1,48 @@
+package project.repository;
+
+import org.springframework.stereotype.Repository;
+import project.member.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findLoginId(String loginId);
+    List<Member> findAll();
+
+
+
+//    private static Map<Long, Member> store = new ConcurrentHashMap<>();
+//    private static long sequence = 0L;
+//
+//    public Member findById(Long id) {
+//        return store.get(id);
+//    }
+//
+//    public Member save(Member member) {
+//        member.setId(++sequence);
+//        log.info("save: member={}", member);
+//        store.put(member.getId(), member);
+//
+//        return member;
+//    }
+//
+//    public Optional<Member> findLoginId(String loginId) {
+//        return findAll().stream()
+//                .filter(member -> member.getLoginId().equals(loginId))
+//                .findFirst();
+//    }
+//
+//    public List<Member> findAll() {
+//        return new ArrayList<>(store.values());
+//    }
+//
+//    // 테스트시 초기화를 위해
+//    public void clearStore() {
+//        store.clear();
+//    }
+}
